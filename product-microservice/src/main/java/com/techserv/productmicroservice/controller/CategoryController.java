35ea9226fc6a782d31aa66faa98006d7989c7f60
luis <luxo.lyr@gmail.com>
2023-11-15 02:26:19 -0300

    ""

--- product-microservice/src/main/java/com/techserv/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/techserv/productmicroservice/controller/CategoryController.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +14,12 @@ import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/categories")
-@Slf4j
+@RefreshScope
 public class CategoryController {
 
 
     @Autowired
     private Constants constants;
-
-    @PostConstruct
-    public void init(){
-        log.info("AA :{}",constants);
-    }
-
 
     @GetMapping("/test-prop")
     private String getTestProp(){
