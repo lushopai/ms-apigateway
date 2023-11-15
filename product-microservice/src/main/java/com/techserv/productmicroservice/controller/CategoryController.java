b8770a9851a59518e1fe3956cecd562373903e9a
luis <luxo.lyr@gmail.com>
2023-11-15 02:22:57 -0300

    fix

--- product-microservice/src/main/java/com/techserv/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/techserv/productmicroservice/controller/CategoryController.java
@@ -2,24 +2,32 @@ package com.techserv.productmicroservice.controller;
 
 import com.techserv.productmicroservice.config.Constants;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/categories")
+@Slf4j
 public class CategoryController {
 
 
     @Autowired
     private Constants constants;
 
+    @PostConstruct
+    public void init(){
+        log.info("AA :{}",constants);
+    }
 
 
-    //@GetMapping("/test-prop")
+    @GetMapping("/test-prop")
     private String getTestProp(){
-        return constants.getActuator().get("testProp");
+        return "hOLA";
     }
 }
